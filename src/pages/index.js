fb688feb115406201551288d91d436581b3b0910
Rayhan Islam <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2021-05-23 12:59:50 +0600

    1st day push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,31 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ Products }) {
   return (
-    <div>
+    <div className="bg-grey-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       <Header />
-      <div>
-        {/* banner */}
-        {/* Product */}
+      <div className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed products={Products} />
       </div>
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const Products = await res.json();
+
+  return {
+    props: {
+      Products,
+    },
+  };
+};
